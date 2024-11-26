f03dbbd2cc66031405cdb1903814850cb7c93eea
paulo.hsantos26 <paulo.hsantos26@AME0556336W10-1>
2024-11-26 16:57:51 -0300

    Adicionar arquivos de projeto.

---
+++ Atividades/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Escreva um programa que verifique se um número fornecido pelo usuário é positivo, negativo ou zero.
+        }
+    }
+}
